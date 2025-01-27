775f68cf6b694b329bbd8b44e3911bf17b7501d0
Pujan-khunt <pujankhunt2412@gmail.com>
2025-01-27 11:29:46 +0530

    integrate CodeForces API

---
+++ src/popup.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let cf_contests = [];
+    const btn = document.querySelector('#getApi');
+    const displayContests = document.querySelector('#display-contests');
+
+    btn.addEventListener('click', async () => {
+        try {
+            const response = await fetch('https://codeforces.com/api/contest.list');
+            const data = await response.json();
+            populateArray(data);
+            createCards();
+        } catch (error) {
+            console.log(error);
+        }
+    })
+
+    function populateArray(data) {
+        const contestList = data.result;
+        
+        cf_contests = contestList.filter((contest) => {
+            return contest.phase === 'BEFORE';
+        });
+
+        console.log(cf_contests);
+    }
+
+    function createCards() {
+        cf_contests.forEach((contest) => {
+            const container = document.createElement('div');
+            container.setAttribute('class', 'bg-neutral-600 w-full rounded-lg py-2 px-4 mt-4 text-white');
+            container.innerHTML = `
+                ${contest.name}
+            `;
+
+            displayContests.appendChild(container);
+        });
+    }
+});
