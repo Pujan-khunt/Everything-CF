6d192800d9bbeb10ba6bcc41dab9bfaade92a431
Pujan-khunt <pujankhunt2412@gmail.com>
2025-01-27 23:58:13 +0530

    add live countdown and fix time issues

--- src/popup.js
+++ src/popup.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const contestsDisplay = document.querySelector('#contests-display');
   const feedbackDisplay = document.querySelector('#feedback-display');  
   init();
-
   
   function init() {
     fetchContestList();
@@ -58,34 +57,68 @@ document.addEventListener('DOMContentLoaded', () => {
       const container = document.createElement('div');
       container.setAttribute('class', 'bg-gray-900/80 p-4 mb-6 rounded-xl border border-gray-700 relative');
 
-      const contestDate = new Date(contest.startTimeSeconds * 1000);
-      const remainingTime = new Date(contest.relativeTimeSeconds * -1 );
+      const contestDate = getContestDate(contest);
+      const remainingTime = getRemainingTime(contest);
       
-      const days = Math.floor(remainingTime / (24 * 60 * 60));
-      const hours = remainingTime.getUTCHours().toString();
-      const minutes = remainingTime.getUTCMinutes().toString();
-      const seconds = remainingTime.getUTCSeconds().toString();
-      
-      const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-      const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
       container.innerHTML = `
-        <div class="flex items-center justify-center">
-          <h3 class="text-white text-lg font-semibold">${contest.name}</h3>
-        </div>
-  
-        <div class="mt-4 border-t border-gray-700 pt-4 text-center">
-          <p class="text-gray-400 text-sm">${weekDays[contestDate.getDay()]} &middot; ${months[contestDate.getMonth()]} ${contestDate.getDate()}, ${contestDate.getFullYear()} &middot; ${contestDate.getHours()}:${contestDate.getMinutes()} IST</p>
-          <p class="text-gray-500 text-s mt-2">${days}:${hours}:${minutes}:${seconds}</p>
-        </div>
-  
-        <div class="mt-4 flex items-end justify-between">
-          <div class="text-sm text-green-900 bg-green-600 px-4 py-1 rounded-lg ">Registration Open &#10004;</div>
-          <a href="https://codeforces.com/contests/${contest.id}" class="transform transition active:scale-110 ease-in-out cursor-pointer px-4 py-1.5 bg-gradient-to-r from-blue-500 to-purple-500 text-white rounded-lg shadow text-lg">Link</a>
-        </div>
+      <div class="flex items-center justify-center">
+      <h3 class="text-white text-lg font-semibold">${contest.name}</h3>
+      </div>
+      
+      <div class="mt-4 border-t border-gray-700 pt-4 text-center">
+      <p class="text-gray-400 text-sm">${contestDate.day} &middot; ${contestDate.month} ${contestDate.date}, ${contestDate.year} &middot; ${contestDate.hours}:${contestDate.minutes} IST</p>
+      <p id="contest-timer-display" class="text-gray-500 text-lg mt-2">${remainingTime.days}:${remainingTime.hours}:${remainingTime.minutes}:${remainingTime.seconds}</p>
+      </div>
+      
+      <div class="mt-4 flex items-end justify-between">
+      <div class="text-sm text-green-900 bg-green-600 px-4 py-1 rounded-lg ">Registration Open &#10004;</div>
+      <a href="https://codeforces.com/contests/${contest.id}" target="_blank" class="transform transition active:scale-110 ease-in-out cursor-pointer px-4 py-1.5 bg-gradient-to-r from-blue-500 to-purple-500 text-white rounded-lg shadow text-lg">Link</a>
+      </div>
       `;
-
+      
       contestsDisplay.appendChild(container);
+      setInterval(() => {
+        updateTimer(contest);
+      }, 1000);
     })
+  }
+  
+  function getContestDate(contest) {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const contestDate = new Date(contest.startTimeSeconds * 1000);
+
+    const obj = {
+      day: weekDays[contestDate.getDay()],
+      month: months[contestDate.getMonth()],
+      date: contestDate.getDate().toString().padStart(2, '0'),
+      year: contestDate.getFullYear().toString().padStart(2, '0'),
+      hours: contestDate.getHours().toString().padStart(2, '0'),
+      minutes: contestDate.getMinutes().toString().padStart(2, '0')
+    }
+
+    return obj;
+  }
+
+  function getRemainingTime(contest) {
+    const currentDate = Date.now();
+    const startingDate = contest.startTimeSeconds * 1000;
+    
+    const difference = startingDate - currentDate;
+    
+    const obj = {
+      seconds: Math.floor((difference / 1000) % 60),
+      minutes: Math.floor((difference / (1000 * 60)) % 60),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+    }
+
+    return obj;
+  }
+
+  function updateTimer(contest) {
+    const contestTimerDisplay = document.querySelector('#contest-timer-display');
+    const remainingTime = getRemainingTime(contest);
+    contestTimerDisplay.innerHTML = `${remainingTime.days}:${remainingTime.hours}:${remainingTime.minutes}:${remainingTime.seconds}`;
   }
 })
